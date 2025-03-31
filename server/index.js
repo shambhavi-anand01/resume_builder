3a309bf44ff83a13d7b253e4118d5f094578bedd
Shambhavi Anand <125148718+shambhavi-anand01@users.noreply.github.com>
2025-03-31 20:46:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173", "https://resume-builder-rose-zeta.vercel.app", "https://resume-builder-shambhavi-anands-projects.vercel.app", "https://resume-builder-git-master-shambhavi-anands-projects.vercel.app"],
+    origin: ["http://localhost:5174", "http://localhost:5173", "https://resume-builder-git-master-shambhavi-anands-projects.vercel.app", "https://resume-builder-shambhavi-anands-projects.vercel.app", "https://resume-builder-eight-rose.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow credentials (cookies, authorization headers)
